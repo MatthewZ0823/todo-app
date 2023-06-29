4dfdefcedb44537a198e1679b2bafd5854409b5a
Matthew Zhou <emzeegotanemail2@gmail.com>
2023-06-29 15:18:49 -0400

    Simple task mock up

---
+++ src/lib/Task.svelte
@@ -0,0 +1,10 @@
+<div class='py-1 flex flex-row items-center'>
+  <div class='mx-2 w-6 h-6 bg-transparent border-2 border-blue-300 rounded-md'/>
+  <h1 class='text-slate-300 text-lg'>Example Task</h1>
+</div>
+
+<style>
+  h1 {
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
